add86ef4945eb1c8daaef4fba91f3c3990d0a0f4
xiaozhidong <xiaozhidong@skyworth.com>
2021-09-28 09:44:32 +0800

    Leetcode 1.两数之和TwoSum

---
+++ 1.数组&双指针/A_TwoSum.java
@@ -0,0 +1,47 @@
+package com.example.mvcdemo.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1.两数之和
+ * 题目描述：给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ *
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ *
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ *
+ * 示例 3：
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ */
+
+
+/**
+ * 思路：遍历数组，并使用hash存储值和下标
+ */
+public class A_TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for(int i=0;i<nums.length;i++){
+            int temp = target - nums[i];
+            if(map.containsKey(temp)){
+                res[0] = i;
+                res[1] = map.get(temp);
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return res;
+    }
+}
